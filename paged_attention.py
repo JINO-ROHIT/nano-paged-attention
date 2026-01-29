0f0065d877dd000e90a6a942d84a908620d60696
JINO-ROHIT <find.jinorohit@gmail.com>
2026-01-29 21:50:01 +0530

    fix: map physical page to the id

--- paged_attention.py
+++ paged_attention.py
@@ -10,59 +10,56 @@ class Config:
     head_dim: int = 128
 
     number_of_pages: int = 10
+    page_size: int = 10
 
 
 @dataclass
 class Page:
     page_size: int = 10
     ref_count: int = 0
-    hash: str = uuid.uuid4().hex
-
+    hash: str = field(default_factory = lambda: uuid.uuid4().hex)
 
     def __repr__(self):
         return f"{self.hash} the page size is {self.page_size} and referenced {self.ref_count} times."
 
-    def __post__init__(self):
-        self.kv = torch.zeros(2, page_size, Config.num_head, Config.head_dim)
+    def __post_init__(self):
+        self.kv = torch.zeros(2, self.page_size, Config.num_head, Config.head_dim)
+    
+    def __hash__(self):
+        return hash(self.hash)
 
 @dataclass
 class PageTable:
     map: dict = field(default_factory = dict)
 
     def __repr__(self):
-        return f"the page table has {self.map} entries"
+        return f"the page table has {len(self.map)} entries"
 
-    def map_page(self, logical_id: int, physical_id: int):
-        """map the logical and physical id"""
-        self.map[logical_id] = physical_id
+    def map_page(self, logical_id: int, physical_page: Page):
+        """map the logical page id to physical page"""
+        self.map[logical_id] = physical_page
 
-#to-do    
 class BlockManager:
     def __init__(self, num_pages: int, page_size: int):
         self.pages = [Page(page_size) for _ in range(num_pages)]
-        self.allocated = deque()
         self.free = deque(self.pages) # at first, all the pages are free
+        self.allocated = set()
     
     def allocate(self):
         if not self.free:
             return "all the pages are occupied"
-        page_to_be_allocated = self.free.popleft()
+        page_to_be_allocated = self.free.popleft() # remove the left side page
         page_to_be_allocated.ref_count += 1
-        self.allocated.append(page_to_be_allocated) # remove the left side page
+        self.allocated.add(page_to_be_allocated)
+        return page_to_be_allocated
     
-    def deallocate(self, page_id):
-        if page_id in self.allocated_pages:
-            self.pages[page_id].ref_count -= 1
-            if self.pages[page_id].ref_count == 0:
-                self.allocated_pages.popleft(page_id)
-                self.free.append(page_id)
-
-if __name__ == '__main__':
-    pg = Page(page_size = 2, ref_count = 0)
-    print(pg)
-
-    pg_table = PageTable()
-    pg_table.map_page(0, 100)
-    pg_table.map_page(1, 200)
-    print(pg_table)
-
+    def deallocate(self, page: Page):
+        if page not in self.allocated:
+            return  
+        
+        page.ref_count -= 1
+        
+        if page.ref_count == 0:
+            self.allocated.remove(page)
+            self.free.append(page)
+            page.kv.zero_() # clear the kv cache
